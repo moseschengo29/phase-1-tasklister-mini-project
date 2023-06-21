134effc42cc02cc20e66249b50eaf45e7cce39e9
Moses <mochengo29@gmail.com>
2023-06-21 12:55:09 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToDo(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function addToDo(todo) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", (event) => {
+    deleteHandler(event);
+  });
+  btn.textContent = "x";
+  li.textContent = `${todo} `;
+  li.appendChild(btn);
+  let list = document.querySelector("#tasks");
+  list.appendChild(li);
+  // console.log(list)
+}
+
+function deleteHandler(e) {
+  e.target.parentNode.remove();
+}
